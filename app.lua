ed86e51a1fb6009ccc5e2064af806768e0085f3b
Bryce Mecum <petridish@gmail.com>
2018-07-29 02:20:19 -0800

    Switch out postgresl for mongodb

--- app.lua
+++ app.lua
@@ -3,65 +3,11 @@ local respond_to
 respond_to = require("lapis.application").respond_to
 local json_params
 json_params = require("lapis.application").json_params
-local db = require("lapis.db")
-local Model, enum
-do
-  local _obj_0 = require("lapis.db.model")
-  Model, enum = _obj_0.Model, _obj_0.enum
-end
-local Characters
-do
-  local _class_0
-  local _parent_0 = Model
-  local _base_0 = {
-    url_params = function(self, req, ...)
-      return "character", {
-        server = self.server,
-        name = self.name
-      }
-    end
-  }
-  _base_0.__index = _base_0
-  setmetatable(_base_0, _parent_0.__base)
-  _class_0 = setmetatable({
-    __init = function(self, ...)
-      return _class_0.__parent.__init(self, ...)
-    end,
-    __base = _base_0,
-    __name = "Characters",
-    __parent = _parent_0
-  }, {
-    __index = function(cls, name)
-      local val = rawget(_base_0, name)
-      if val == nil then
-        local parent = rawget(cls, "__parent")
-        if parent then
-          return parent[name]
-        end
-      else
-        return val
-      end
-    end,
-    __call = function(cls, ...)
-      local _self_0 = setmetatable({}, _base_0)
-      cls.__init(_self_0, ...)
-      return _self_0
-    end
-  })
-  _base_0.__class = _class_0
-  local self = _class_0
-  self.primary_key = {
-    "server",
-    "name"
-  }
-  self.timestamp = true
-  if _parent_0.__inherited then
-    _parent_0.__inherited(_parent_0, _class_0)
-  end
-  Characters = _class_0
-end
+local mongo = require('mongo')
+local client = mongo.Client('mongodb://127.0.0.1')
+local characters = client:getCollection('treestats-dev', 'characters')
 local servers = {
-  'WintersEbb'
+  "TestServer"
 }
 local App
 do
@@ -71,18 +17,15 @@ do
     [{
       index = "/"
     }] = function(self)
-      return self:html(function()
-        h2("Index")
-        return a({
-          href = self:url_for("servers")
-        }, "List servers")
-      end)
+      return {
+        render = true
+      }
     end,
     [{
       characters = "/characters"
     }] = respond_to({
       GET = function(self)
-        self.characters = db.query("select * from characters")
+        self.characters = characters:find(mongo.BSON('{}'))
         return {
           render = true
         }
@@ -100,7 +43,7 @@ do
     [{
       servers = "/servers"
     }] = function(self)
-      self.servers = db.query("select distinct server from characters limit 100")
+      self.servers = servers
       return {
         render = true
       }
@@ -108,28 +51,15 @@ do
     [{
       server = "/server/:server"
     }] = function(self)
-      local res = db.query("select * from characters where server = ?", self.params.server)
-      return self:html(function()
-        h2("Character Listing for " .. tostring(self.params.server))
-        return ul(function()
-          for i = 1, #res do
-            li(function()
-              return a({
-                href = self:url_for("character", {
-                  server = self.params.server,
-                  name = res[i].name
-                })
-              }, res[i].name)
-            end)
-          end
-        end)
-      end)
+      self.characters = characters:find(mongo.BSON('{ "s" : "TestServer" }'))
+      return {
+        render = true
+      }
     end,
     [{
       character = "/server/:server/:name"
     }] = function(self)
-      self.character = db.query("select server, name from characters where server = ? and name = ? limit 1", self.params.server, self.params.name)
-      print("@character is " .. tostring(self.character))
+      self.character = characters:findOne(mongo.BSON('{ "s" : "TestServer", "n" : "TestCharacter"}')):value()
       return {
         render = true
       }
--- app.moon
+++ app.moon
@@ -1,34 +1,24 @@
 lapis = require "lapis"
+
 import respond_to from require "lapis.application"
 import json_params from require "lapis.application"
 
-db = require "lapis.db"
+mongo = require 'mongo'
+client = mongo.Client('mongodb://127.0.0.1')
+characters = client\getCollection('treestats-dev', 'characters')
 
-
-import Model, enum from require "lapis.db.model"
-
-class Characters extends Model
-  @primary_key: { "server", "name" }
-  @timestamp: true
-  url_params: (req, ...) =>
-    "character", { server: @server, name: @name }
-
--- TODO: Factor out
-servers = {
-  'WintersEbb'
-}
+-- globals?
+servers = { "TestServer" }
 
 class App extends lapis.Application
   @enable "etlua"
 
   [index: "/"]: =>
-    @html ->
-      h2 "Index"
-      a href: @url_for("servers"), "List servers"
+    render: true
 
   [characters: "/characters"]: respond_to {
     GET: =>
-      @characters = db.query "select * from characters"
+      @characters = characters\find(mongo.BSON('{}'))
       render: true
 
     POST: json_params => {
@@ -41,20 +31,13 @@ class App extends lapis.Application
   }
 
   [servers: "/servers"]: =>
-    @servers = db.query "select distinct server from characters limit 100"
+    @servers = servers
     render: true
 
   [server: "/server/:server"]: =>
-    res = db.query "select * from characters where server = ?", @params.server
-
-    @html ->
-      h2 "Character Listing for #{@params.server}"
-      ul ->
-        for i = 1, #res
-          li ->
-            a href: @url_for("character", server: @params.server, name: res[i].name), res[i].name
+    @characters = characters\find(mongo.BSON('{ "s" : "TestServer" }'))
+    render: true
 
   [character: "/server/:server/:name"]: =>
-    @character = db.query "select server, name from characters where server = ? and name = ? limit 1", @params.server, @params.name
-    print "@character is #{@character}"
+    @character = characters\findOne(mongo.BSON('{ "s" : "TestServer", "n" : "TestCharacter"}'))\value()
     render: true
